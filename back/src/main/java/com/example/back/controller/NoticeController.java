2290107caf79d8560d608fcde41ba74d830634ed
J-Been <tjdqls1054@naver.com>
2023-09-07 23:36:24 +0900

    updateNotice 수정 전

--- back/src/main/java/com/example/back/controller/NoticeController.java
+++ back/src/main/java/com/example/back/controller/NoticeController.java
@@ -4,9 +4,12 @@ import com.example.back.dto.NoticeDto;
 import com.example.back.entity.Notice;
 import com.example.back.mapper.NoticeMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/notice")
@@ -28,22 +31,22 @@ public class NoticeController {
     }
 
     //공지사항 상세보기 조회
-    ////@PathVariable 활용 문의 필요
-    @GetMapping("/{id}")
-    public void detailNotice(@PathVariable Long id) {
-        noticeMapper.selectNoticeDetail(id);
+    @GetMapping("/{noticeId}")
+    public ResponseEntity<?> detailNotice(@PathVariable Long noticeId) {
+        Map<String, Object> result = noticeMapper.selectNoticeDetail(noticeId);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     //공지사항 수정
     @PostMapping("/update")
     public void updateNotice(@RequestBody NoticeDto noticeDto) {
         noticeMapper.updateNotice(noticeDto);
+
     }
 
     //공지사항 삭제
-    ////@PathVariable 활용 문의 필요
-    @DeleteMapping("/{id}")
-    public void deleteNotice(@PathVariable Long id) {
-        noticeMapper.deleteNotice(id);
+    @DeleteMapping("/{noticeId}")
+    public void deleteNotice(@PathVariable Long noticeId) {
+        noticeMapper.deleteNotice(noticeId);
     }
 }
--- back/src/main/java/com/example/back/mapper/NoticeMapper.java
+++ back/src/main/java/com/example/back/mapper/NoticeMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 @Repository
@@ -14,10 +15,10 @@ public interface NoticeMapper {
 
     List<NoticeDto> selectNoticeList(); //공지사항 목록 조회
 
-    NoticeDto selectNoticeDetail(Long id); //공지사항 상세보기 조회
+    Map<String, Object> selectNoticeDetail(Long noticeId); //공지사항 상세보기 조회
 
     void updateNotice(NoticeDto noticeDto); //공지사항 수정
 
-    void deleteNotice(Long id); //공지사항 삭제
+    void deleteNotice(Long noticeId); //공지사항 삭제
 
 }
