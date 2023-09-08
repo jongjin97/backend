2c9b55a8a09923489f632555b53f84a764cd2f74
ektkfk180 <ektkfk180@naver.com>
2023-09-08 13:24:56 +0900

    valid 추가

--- back/src/main/java/com/example/back/dto/NoticeDto.java
+++ back/src/main/java/com/example/back/dto/NoticeDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,8 +15,10 @@ public class NoticeDto {
 
     private Long id;
 
+    @NotEmpty(message = "제목은 필수 입력 값입니다.")
     private String noticeTitle; //제목
 
+    @NotEmpty(message = "내용은 필수 입력 값입니다.")
     private String noticeContents; //내용
 
 }
