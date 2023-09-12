270cdff1ccf858192d063ac026b9210d80ea8541
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-09-12 23:32:25 +0900

    TopicMapper 추가

---
+++ back/src/main/java/com/example/back/mapper/TopicMapper.java
@@ -0,0 +1,23 @@
+package com.example.back.mapper;
+
+import com.example.back.dto.TopicDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+@Repository
+public interface TopicMapper {
+
+    void createTopic(TopicDto topicDto); // 관심 주제 추가
+
+    List<Map<String, Object>> selectTopicList(); // 관심 주제 목록 조회
+
+    Map<String, Object> selectTopicDetail(Long selectProductId); // 관심 주제 상세보기 조회
+
+    void updateTopic(TopicDto topicDto); // 관심 주제 수정
+
+    void deleteTopic(Long selectProductId); // 관심 주제 삭제
+}
