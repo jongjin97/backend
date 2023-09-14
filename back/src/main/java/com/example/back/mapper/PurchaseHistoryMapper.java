4599660861967bad79b063218b14a946acfef376
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-09-15 01:57:11 +0900

    PurchaseHistoryMapper 추가

---
+++ back/src/main/java/com/example/back/mapper/PurchaseHistoryMapper.java
@@ -0,0 +1,23 @@
+package com.example.back.mapper;
+
+import com.example.back.dto.PurchaseHistoryDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+@Repository
+public interface PurchaseHistoryMapper {
+
+    void createPurchaseHistory(PurchaseHistoryDto purchaseHistoryDto); // 구매 이력 추가
+
+    List<Map<String,Object>> purchaseHistories(); // 구매 이력 목록 조회
+
+    Map<String, Object> selectPurchaseHistoryDetail(Long purchaseId); // 구매 이력 상세 조회
+
+    void updatePurchaseHistory(PurchaseHistoryDto purchaseHistoryDto); // 구매 이력 수정
+
+    void deletePurchaseHistory(Long purchaseId); // 구매 이력 삭제
+}
