ee7a03d62909a5cf12b08f18cfe523c4cb90dfde
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-09-15 01:54:48 +0900

    PurchasehistoryDto 추가

--- back/src/main/java/com/example/back/dto/PurchasehistoryDto.java
+++ back/src/main/java/com/example/back/dto/PurchasehistoryDto.java
@@ -1,4 +1,20 @@
 package com.example.back.dto;
 
-public class PurchasehistoryDto {
+import com.example.back.entity.Product;
+import com.example.back.entity.User;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PurchaseHistoryDto {
+
+    private Long id;
+
+    private User user; // 구매 유저
+
+    private Product product; // 구매 상품
+
 }
