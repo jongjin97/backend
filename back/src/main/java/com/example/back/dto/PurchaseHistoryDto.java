5f7c7b900293a277b2c3f8c2e0022328b9e915c4
jaewoo Lim <130232709+jaeuu@users.noreply.github.com>
2023-10-09 00:57:52 +0900

    PurchaseHistoryDto 수정

--- back/src/main/java/com/example/back/dto/PurchaseHistoryDto.java
+++ back/src/main/java/com/example/back/dto/PurchaseHistoryDto.java
@@ -1,8 +1,6 @@
 package com.example.back.dto;
 
 import com.example.back.config.auth.PrincipalDetail;
-import com.example.back.entity.Product;
-import com.example.back.entity.User;
 import lombok.*;
 
 @Getter
@@ -14,16 +12,16 @@ public class PurchaseHistoryDto {
 
     private Long id;
 
-    private User user; // 구매 유저
+    private Long productId; // product_id
 
-//    private Long productId; // product_id / 재우
+    private Long userId; // user_id
 
-    //private Product product; // 구매 상품 / 이건 없어도됨
+    public Long getUserId() {
+        return userId;
+    }
 
-    private Long principalDetail; // 홍진
-
-    public Long getUserId(PrincipalDetail principalDetail) { // 홍진
-        return this.principalDetail = principalDetail.getId();
+    public void setUserId(PrincipalDetail principalDetail) {
+        this.userId = principalDetail.getId();
     }
 
 }
