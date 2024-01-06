b7514afd608dd694bf2606496d14384634cd9b8f
jongjin97 <kakao1302@naver.com>
2024-01-07 02:31:55 +0900

    ChatController, Socket 설정

--- back/src/main/java/com/example/back/config/WebSocketConfig.java
+++ back/src/main/java/com/example/back/config/WebSocketConfig.java
@@ -4,24 +4,82 @@ package com.example.back.config;
 import com.example.back.handler.SocketHandler;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.socket.config.annotation.*;
+
+import java.util.Objects;
 
 /**
  * 1:1 채팅 기능에 필요한 웹소켓
  */
 
+//@Configuration
+//@EnableWebSocket
+//@RequiredArgsConstructor
+//public class WebSocketConfig implements WebSocketConfigurer {
+//
+//    private final SocketHandler socketHandler;
+//
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(socketHandler, "/chating");
+//    }
+//}
 @Configuration
-@EnableWebSocket
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    private final SocketHandler socketHandler;
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtProvider jwtProvider;
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // 메시지 보낼 받을 때 경로
+        config.enableSimpleBroker("/sub");
+        // 메시지 보낼 때 경로
+        config.setApplicationDestinationPrefixes("/room");
+    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/chating");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Stomp Socket 엔드포인트설정
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    //Socket token 확인
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                // Socket 처음 연결 시
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String token = Objects.requireNonNull(accessor.getNativeHeader("Authorization")).get(0);
+                    if(jwtProvider.validateToken(token)){
+                        token = token.split(" ")[1].trim();
+                        Authentication user = jwtProvider.getAuthentication(token);
+
+                        accessor.setUser(user);
+                    }
+                }
+                return message;
+            }
+        });
     }
 }
---
+++ back/src/main/java/com/example/back/controller/ChatController.java
@@ -0,0 +1,35 @@
+package com.example.back.controller;
+
+import com.example.back.config.auth.PrincipalDetail;
+import com.example.back.entity.User;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.socket.WebSocketMessage;
+
+import java.security.Principal;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/{roomId}")
+    @SendTo("/sub/{roomId}")
+    public String sendMessage(@DestinationVariable String roomId, @Payload String message
+            , SimpMessageHeaderAccessor headerAccessor) {
+        Authentication authentication = (Authentication) headerAccessor.getUser();
+        PrincipalDetail principalDetail = (PrincipalDetail) authentication.getPrincipal();
+        User user = principalDetail.getUser();
+        User user2 = ((PrincipalDetail) authentication.getPrincipal()).getUser();
+        System.out.println();
+        return message;
+    }
+}
